c38aaa1bb5041211e03ab9d4c27d35859d4af36f
Min-Su Kim <min-sukim@Min-Suui-MacBookPro-2.local>
2022-01-24 12:13:20 +0900

    ✨ 공유하기 기능 추가

--- Oneboard/Source/ViewController/Clipboard/ClipboardViewController+TableView.swift
+++ Oneboard/Source/ViewController/Clipboard/ClipboardViewController+TableView.swift
@@ -23,15 +23,35 @@ extension ClipboardViewController: UITableViewDataSource, UITableViewDelegate {
         cell.longClick = { [weak self] in
             let alert = UIAlertController(title: nil, message: clipboard.contents, preferredStyle: UIAlertController.actionSheetForiPad)
             alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "공유하기", style: .default, handler: { _ in
+                self?.shareClipboard(clipboard)
+            }))
             alert.addAction(UIAlertAction(title: "삭제", style: .destructive, handler: { _ in
-                ClipboardManager.remove(clipboard)
-                self?.fetchClipboards()
+                self?.removeClipboard(clipboard)
             }))
             self?.present(alert, animated: true, completion: nil)
         }
         return cell
     }
     
+    func shareClipboard(_ clipboard: Clipboard) {
+        var shareObject = [Any]()
+        shareObject.append(clipboard.contents ?? "")
+        
+        let activityViewController = UIActivityViewController(activityItems : shareObject, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        activityViewController.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed: Bool, arrayReturnedItems: [Any]?, error: Error?) in
+            if completed {
+                self.view.makeToast("전송되었습니다.")
+            }
+        }
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    func removeClipboard(_ clipboard: Clipboard) {
+        ClipboardManager.remove(clipboard)
+        self.fetchClipboards()
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         VibrateManager.changeOrSelectVibrate()
--- Oneboard/Source/ViewController/Main/MainViewController+Collection.swift
+++ Oneboard/Source/ViewController/Main/MainViewController+Collection.swift
@@ -63,16 +63,33 @@ extension MainViewController: MainListCollectionViewCellDelegate {
         
         let alert = UIAlertController(title: subject, message: nil, preferredStyle: UIAlertController.actionSheetForiPad)
         alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "공유하기", style: .default, handler: { _ in
+            self.shareCopy(item)
+        }))
         alert.addAction(UIAlertAction(title: "편집", style: .default, handler: { _ in
-            self.editCopy(item: item)
+            self.editCopy(item)
         }))
         alert.addAction(UIAlertAction(title: "삭제", style: .destructive, handler: { _ in
-            self.removeCopy(item: item)
+            self.removeCopy(item)
         }))
         present(alert, animated: true, completion: nil)
     }
     
-    func editCopy(item: Copy) {
+    func shareCopy(_ item: Copy) {
+        var shareObject = [Any]()
+        shareObject.append(item.contents ?? "")
+        
+        let activityViewController = UIActivityViewController(activityItems : shareObject, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        activityViewController.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed: Bool, arrayReturnedItems: [Any]?, error: Error?) in
+            if completed {
+                self.view.makeToast("전송되었습니다.")
+            }
+        }
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    func editCopy(_ item: Copy) {
         let vc = AddViewController()
         vc.delegate = self
         vc.isEditMode = true
@@ -80,7 +97,7 @@ extension MainViewController: MainListCollectionViewCellDelegate {
         present(vc, animated: true, completion: nil)
     }
     
-    func removeCopy(item: Copy) {
+    func removeCopy(_ item: Copy) {
         CopyManager.remove(copy: item)
         fetchList()
     }
